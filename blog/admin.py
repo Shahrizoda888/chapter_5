11a0142ee3d5b6caec4d842f37d3cf8a83cc2819
shahrizodasub <shahrizodasubhonova2006@gmail.com>
2021-07-23 14:51:05 +0500

    change to admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Post 
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display=('title','body','author')
+    list_filter=('title','author')
+    search_fields=('title',)
+
+
